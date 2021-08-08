6be24e7c16f325863fd501c3471c8f13aa163d68
Mauricio J Carrillo - Laptop <mauricio.jcarrillo@gmail.com>
2021-08-08 13:57:25 -0500

    Finalización practice N°2

--- figuras.js
+++ figuras.js
@@ -14,13 +14,15 @@ function calcularPerimetroCuadrado() {
     const input = document.getElementById("ladoCuadrado");
     const value = input.value;
     const perimetro = perimetroCuadrado(value);
-    alert("El perímetro es: " + perimetro); 
+    const output = document.getElementById("resultCuadrado");
+    output.innerText = "El perimetro es: " + perimetro;
 }
 function calcularAreaCuadrado() {
     const input = document.getElementById("ladoCuadrado");
     const value = input.value;
     const area = areaCuadrado(value);
-    alert("El área es: " + area); 
+    const output = document.getElementById("resultCuadrado");
+    output.innerText = "El área es: " + area;
 }
 function calcularPerimetroTriangulo() {
     const input1 = document.getElementById("ladoUno");
@@ -30,7 +32,8 @@ function calcularPerimetroTriangulo() {
     const value2 = Number(input2.value);
     const value3 = Number(input3.value);
     const perimetro = perimetroTriangulo(value1, value2, value3);
-    alert("El perímetro es: " + perimetro); 
+    const output = document.getElementById("resultTriangulo");
+    output.innerText = "El perímetro es: " + perimetro;
 }
 function calcularAreaTriangulo() {
     const input1 = document.getElementById("ladoUno");
@@ -40,50 +43,55 @@ function calcularAreaTriangulo() {
     const value2 = Number(input2.value);
     const value3 = Number(input3.value);
     const area = areaTriangulo(value1, value2, value3);
-    alert("El área es: " + area);  
+    const output = document.getElementById("resultTriangulo");
+    output.innerText = "El área es: " + area;
 }
 function calcularPerimetroCirculo() {
     const input = document.getElementById("radio");
     const value = input.value;
     const perimetro = perimetroCirculo(value);
-    alert("El perímetro es: " + perimetro); 
+    const output = document.getElementById("resultCirculo");
+    output.innerText = "El perímetro es: " + perimetro;
 }
 function calcularAreaCirculo() {
     const input = document.getElementById("radio");
     const value = input.value;
     const area = areaCirculo(value);
-    alert("El área es: " + area); 
+    const output = document.getElementById("resultCirculo");
+    output.innerText = "El área es: " + area;
 }
 function validacionTrianguloIsosceles() {
     const input1 = document.getElementById("ladoOne");
     const input2 = document.getElementById("ladoTwo");
     const input3 = document.getElementById("base1");
+    const output = document.getElementById("resultTrianguloIsosceles");
     const value1 = Number(input1.value);
     const value2 = Number(input2.value);
     const value3 = Number(input3.value);
     const value4 = Number(value3 / 2);
     if((value1 || value2) < value4){
-        alert("No es un triangulo isósceles, ni siquiera es un triangulo");
+        output.innerText = "No es un triangulo isósceles, ni siquiera es un triangulo";
     }
     else if(value1 != value2){
-        alert("No es un triangulo isósceles");
+        output.innerText = "No es un triangulo isósceles";
     }
-    else alert("Si es un triangulo isósceles");
+    else output.innerText = "Si es un triangulo isósceles";
 }
 function validacionAlturaTrianguloIsosceles() {
     const input1 = document.getElementById("ladoOne");
     const input2 = document.getElementById("ladoTwo");
     const input3 = document.getElementById("base1");
+    const output = document.getElementById("resultTrianguloIsosceles");
     const value1 = Number(input1.value);
     const value2 = Number(input2.value);
     const value3 = Number(input3.value);
     const value4 = Number(value3 / 2);
     const altura = Number(Math.sqrt((value1)**2-(value3/2)**2));
     if((value1 || value2) < value4){
-        alert("No es un triangulo isósceles, ni siquiera es un triangulo");
+        output.innerText = "No es un triangulo isósceles, ni siquiera es un triangulo";
     }
     else if(value1 != value2){
-        alert("No es un triangulo isósceles");
+        output.innerText = "No es un triangulo isósceles";
     }
-    else alert("La altura es: " + altura);
+    else output.innerText = "La altura es: " + altura;
 }
